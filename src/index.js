2d217331c00ab7c401e5e81b972d743b0ea128d9
denisbilurka23 <vadya555555@gmail.com>
2021-05-28 11:23:56 +0300

    gh pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './redux/Redux'
 
 
 const app = (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
